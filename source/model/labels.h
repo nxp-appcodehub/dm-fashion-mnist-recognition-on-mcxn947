5d4dbe52f3618c61e74196d73f95c54055f5ee16
tonyzhangnxp <tony.zhang@nxp.com>
2024-01-26 10:04:03 +0800

    Initial commit, version 1.0

---
+++ source/model/labels.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2018 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _LABELS_H_
+#define _LABELS_H_
+
+static const char* labels[] = {
+	"T-shirt", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "ankle boot",
+		"          "
+};
+
+#endif // _LABELS_H_
